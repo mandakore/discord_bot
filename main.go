1b7541a6246bd64a26f7f71c19cc58269dbe8c06
mandakore <galaxyfish11@gmail.com>
2025-08-22 22:13:25 +0900

    first

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os" // osパッケージをインポート
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv" // godotenvパッケージをインポート
+)
+
+func main() {
+
+
+
+}
